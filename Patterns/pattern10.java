2e4924c22aab4dc2937e31297566ab08b98e4871
abhishekck31 <abhishekkannur31@gmail.com>
2025-06-11 22:52:35 +0530

    pattern10

---
+++ Patterns/pattern10.java
@@ -0,0 +1,31 @@
+public class pattern10 {
+    public static void pattern() {
+        int n = 5; // height of the top half
+
+        // Top half
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Bottom half
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pattern();
+    }
+}
