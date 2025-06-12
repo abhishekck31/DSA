164aa70b3cef788930aede008e684333abb36c40
abhishekck31 <abhishekkannur31@gmail.com>
2025-06-12 08:34:22 +0530

    pattern11

---
+++ Patterns/pattern11.java
@@ -0,0 +1,24 @@
+public class pattern11 {
+    public static void pattern(int n){
+        int space = 2*(n-1);
+        for(int i=1;i<=n;i++){
+           //numbers
+           for(int j=1;j<=i;j++){
+            System.out.print(j);
+           }
+           //space
+           for(int j=1;j<=space;j++){
+            System.out.print(" ");
+           }
+           //numbers
+           for(int j=i;j>=1;j--){
+            System.out.print(j);
+           }
+           System.out.println("");
+           space -=2;
+        }
+    }
+    public static void main(String[] args){
+        pattern(5);
+    }
+}
