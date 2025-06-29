131cbe6663f54d279da7ad7ed503ba9d5b0af4c7
abhishekck31 <abhishekkannur31@gmail.com>
2025-06-29 16:31:08 +0530

    patterns revision

--- patternsrevision/pattern3.java
+++ patternsrevision/pattern3.java
@@ -1,7 +1,7 @@
 public class pattern3 {
     public static void pattern(int n){
         for(int i=0;i<=n;i++){
-            for(int j=0;j<=i;j++){
+            for(int j=1;j<=i;j++){
                 System.out.print(j);
             }
             System.out.println("");
