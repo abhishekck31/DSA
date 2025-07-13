43478eb34242a60c25df21c1964211558e33679f
abhishekck31 <abhishekkannur31@gmail.com>
2025-07-13 14:31:19 +0530

    ArrayList

---
+++ Arraylist.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Arraylist{
+    public static void main(String[] args) {
+        ArrayList<Integer>aList = new ArrayList<>();
+        aList.add(4);
+        aList.add(5);
+        System.out.println(aList);
+        System.out.println(aList.indexOf(5));
+        System.out.println(aList.size());
+    }
+}
