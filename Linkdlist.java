36a22f0b1c664effd57554645d1482e071ee9154
abhishekck31 <abhishekkannur31@gmail.com>
2025-07-13 14:57:17 +0530

    linkedList

---
+++ Linkdlist.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Linkdlist {
+    public static void main(String[] args){
+        LinkedList<Integer>ll = new LinkedList<>();
+        ll.add(4);
+        ll.addFirst(5);
+        ll.addLast(3);
+        System.out.println(ll);
+    }
+}
