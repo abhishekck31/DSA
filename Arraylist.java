ba043124fe3f0e497ff1a95b78cc1cdcfd1998de
abhishekck31 <abhishekkannur31@gmail.com>
2025-08-03 14:15:29 +0530

    ArrayList

--- Arraylist.java
+++ Arraylist.java
@@ -1,11 +1,9 @@
 import java.util.*;
-public class Arraylist{
+
+public class Arraylist {
     public static void main(String[] args) {
-        ArrayList<Integer>aList = new ArrayList<>();
-        aList.add(4);
-        aList.add(5);
-        System.out.println(aList);
-        System.out.println(aList.indexOf(5));
-        System.out.println(aList.size());
+        List<String> nameslist = new ArrayList<>();
+        nameslist.add("Abhishek");
+        System.out.println(nameslist.get(0));
     }
 }
