09e8b819c500d6b097204734b72808741893f839
abhishekck31 <abhishekkannur31@gmail.com>
2025-08-13 08:54:49 +0530

    if else conditions

---
+++ Basics/conditons2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class conditons2 {
+    public static void grading(){
+        Scanner sc  = new Scanner(System.in);
+        System.out.print("Enter your marks: ");
+        int marks = sc.nextInt();
+        sc.close();
+
+        if (marks >= 90){
+            System.out.println("GRADE A");
+        }else {
+            if(marks >=70){
+                System.out.println("GRADE B");
+            } else {
+                if(marks >=50){
+                    System.out.println("GRADE C");
+                }else{
+                    System.out.println("GRADE D");
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+        grading();
+    }
+}
