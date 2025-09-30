ee727ad2beae7643242715768a8fd38aa847af4d
abhishekck31 <abhishekkannur31@gmail.com>
2025-09-30 09:46:23 +0530

    First One

--- BinarySearch/first1.java
+++ BinarySearch/first1.java
@@ -1,18 +1,20 @@
 public class first1 {
-    public static int binarySearch(int arr[],int target,int low,int high){
-        while(low<=high){
+    public static int binarySearch(int arr[], int target, int low, int high) {
+        int result = -1;
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            if(arr[mid] == target){
-                return mid;
-            }else if(arr[mid] < target){
+            if (arr[mid] == target) {
+                result = mid; 
+                high = mid - 1; 
+            } else if (arr[mid] < target) {
                 low = mid + 1;
-            }else{
+            } else {
                 high = mid - 1;
             }
         }
-        return -1;
+        return result; 
     }
-    public static int firstone(int[] arr,int target){
+    public static int firstone(int[] arr, int target) {
         int low = 0;
         int high = 1;
         while (high < arr.length && target > arr[high]){
@@ -21,13 +23,15 @@ public class first1 {
         }
         return binarySearch(arr, target, low, Math.min(high, arr.length - 1));
     }
+    
     public static void main(String[] args){
         int arr[] ={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1};
         int target = 1;
         int result = firstone(arr, target);
+        
         if(result != -1){
             System.out.println("First occurrence of " + target + " is at index: " + result);
-    }     else{
+        } else{
             System.out.println("Element not found");
         }
     }
